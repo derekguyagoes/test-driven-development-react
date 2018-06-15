93cbeed65a872e8b956bb9753a2d8d75940ee80c
derek <derek.guyagoes@gmail.com>
2018-06-15 13:50:15 -0600

    end of module 3

---
+++ 03/demos/demos/Before/hello-world/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <p>this is hte header </p>
+            </div>
+        );
+    }
+}
+
+Header.propTypes = {};
+
+export default Header;
